be9c2bb5bcc8d2a4f9539b97da3b952eef54623b
Ben Pettit <benpettit@digimulti.com>
2013-02-15 15:36:17 +1100

    Added a DownloadOperation class to be built.

---
+++ operations/DownloadOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.h
+//  objective-curl
+//
+//  Created by eggers on 15/02/13.
+//
+//
+
+#import "CurlOperation.h"
+
+@interface DownloadOperation : CurlOperation
+
+@end
---
+++ operations/DownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.m
+//  objective-curl
+//
+//  Created by eggers on 15/02/13.
+//
+//
+
+#import "DownloadOperation.h"
+
+@implementation DownloadOperation
+
+@end
